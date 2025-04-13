b6b9ebf3a708abfb74816aa39e0776dc6d51e51b
Marylyn Lajato <marylyn.lajato@teamhgs.com>
2025-04-13 19:33:50 +0800

    (General) Remove unneeded controllers (1)

--- app/HelperFunctions.php
+++ app/HelperFunctions.php
@@ -1,49 +1,6 @@
 <?php
 namespace App;
-
-use App\Lib\LibUtility;
-use App\Models\Acknowledgement;
-use App\Models\Attachment;
-use App\Models\CarryOverLeave;
-use App\Models\Client;
-use App\Models\ClientUser;
-use App\Models\ClientTeam;
-use App\Models\CssClient;
-use App\Models\Document;
-use App\Models\Employee;
-use App\Models\EmployeeLeave;
-use App\Models\EmployeeSchedule;
-use App\Models\EmployeeSettingItems;
-use App\Models\IpWhitelist;
-use App\Models\Module;
-use App\Models\ModuleRights;
-use App\Models\Option;
-use App\Models\PayrunAbsentLate;
-use App\Models\PayrunNightDifferential;
-use App\Models\PayrunOvertime;
-use App\Models\Policy;
-use App\Models\RoomReservation;
-use App\Models\SystemConfiguration;
-use App\Models\TicketCategory;
-use App\Models\TimeTracker;
-use App\Models\TimeTrackerLine;
-use App\Models\TicketTrackerLine;
-use App\Models\TrainingClassMember;
-use App\Models\UserGroupLine;
-use App\Models\UserSetting;
-use App\Models\UserActivityLog;
-use App\Services\ClientUsers\ServicesClientUsersGeneral;
-use App\Services\EmployeeService;
-use App\User;
-use Carbon\Carbon;
-use Carbon\CarbonImmutable;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Session;
-use DateTime;
 use DB;
-use Spatie\Activitylog\Traits\LogsActivity;
-use NumberFormatter;
 
 
 class HelperFunctions {
