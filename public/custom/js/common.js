c844eb909c344b86b856745692abb0eb2ac70de8
Marylyn Lajato <marylyn.lajato@teamhgs.com>
2025-05-23 21:39:27 +0800

    (ayee2) Fix mobile menu responsiveness (4)

--- public/custom/js/common.js
+++ public/custom/js/common.js
@@ -15,7 +15,7 @@ window.jQuery(document).ready(function () {
          */
         init: function () {
             this.setMobileMenu();
-            this.toggleLoginButton();
+            this.toggleUpdatesMobile();
         },
 
         setMobileMenu: function() {
@@ -33,24 +33,47 @@ window.jQuery(document).ready(function () {
             });
         },
 
-        toggleLoginButton: function() {
+        toggleUpdatesMobile: function() {
             window.jQuery("#btn-step-to-thy-conquest").show();
             window.jQuery("header.top_panel_custom_header-home").css({
                 "height": "100px"
             });
-
             window.jQuery("#frm-login").css({
                 "width": "50%"
             });
+            window.jQuery('<style>')
+                .prop('type', 'text/css')
+                .html(`
+                    .welcome-title::after { top: initial !important; }
+                `)
+                .appendTo('head');
 
             if (/Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
                 window.jQuery("#btn-step-to-thy-conquest").hide();
+
                 window.jQuery("header.top_panel_custom_header-home").css({
                     "height": "50px"
                 });
+
                 window.jQuery("#frm-login").css({
                     "width": "100%"
                 });
+
+                window.jQuery('<style>')
+                    .prop('type', 'text/css')
+                    .html(`
+                        .welcome-title::after { top: 50% !important; }
+                    `)
+                    .appendTo('head');
+
+                window.jQuery('<style>')
+                    .prop('type', 'text/css')
+                    .html(`
+                    .common-paragraph { font-size: 14px !important; }
+                `)
+                    .appendTo('head');
+
+                window.jQuery("#div-design-flag .wbp_reference_only").removeClass("flex-container");
             }
         },
     }
